435e9313a071ead957721f9a15d7e2429f7340d1
Gustavo <guga_floriano13@hotmail.com>
2022-07-19 17:03:32 -0300

    creation global-validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsString()
   email: string;
 
   @IsString()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { validarEmail } from 'src/global-validation/email';
 import { DeleteResult } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -11,16 +12,17 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto): Promise<any> {
     const hasEmail = await this.userRepository.hasEmail(createUserDto.email);
-    if (hasEmail) {
-      throw new BadRequestException('Email already exists');
-    }
+    if (hasEmail) throw new BadRequestException('Email já existe');
+
+    if (!validarEmail(createUserDto.email))
+      throw new BadRequestException('Email inválido');
     return this.userRepository.create(createUserDto);
   }
 
   async getUserbyId(id: number): Promise<UserEntity> {
     const UserNotFound = await this.userRepository.getUserById(id);
     if (!UserNotFound) {
-      throw new BadRequestException('User not exist!');
+      throw new BadRequestException('Usuárior não existe!');
     }
     return UserNotFound;
   }
@@ -30,17 +32,17 @@ export class UsersService {
   }
 
   async deleteUserById(id: number): Promise<DeleteResult> {
-    const UserNotFound = this.userRepository.deleteUserById(id);
-    if (!UserNotFound) {
-      throw new BadRequestException('User not exist!');
+    const user = this.userRepository.deleteUserById(id);
+    if (!user) {
+      throw new BadRequestException('Usuário não existe!');
     }
-    return UserNotFound;
+    return user;
   }
 
   async updateUserById(id: number, update: UpdateUserDto): Promise<UserEntity> {
     const findUser = await this.userRepository.getUserById(id);
     if (!findUser) {
-      throw new BadRequestException('User not exist!');
+      throw new BadRequestException('Usuário não existe!');
     }
     return await this.userRepository.updateUserById(update, id);
   }
